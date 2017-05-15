8094af8e9c989b83fb1c5ea3ab7d07608c63c689
Gus Escola <camagrock@gmail.com>
2017-05-15 17:19:45 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+const app = express()
+
+const PORT = 3000
+
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+app.get('/', (req, res)=> {
+	res.render ('index', {titlePage: 'Super Página'})
+
+})
+
+app.listen (PORT, () => console.log (`Listening on port ${PORT}`))
+
